c4bcec45343da6f1e2514ac7f8709f7a03109f6a
monisha <monishasomashekar2000@gmail.com>
2022-08-01 22:28:30 +0530

    file handler

---
+++ filehandling/append.java
@@ -0,0 +1,26 @@
+package filehandling;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+public class append {
+	public static void main( String[] args ) {
+
+	      try {
+	         String data = " Tutorials Point is a best website in the world";
+	         File f1 = new File("MyFile.txt");
+	         if(!f1.exists()) {
+	            f1.createNewFile();
+	         }
+
+	         FileWriter fileWritter = new FileWriter(f1.getName(),true);
+	         BufferedWriter bw = new BufferedWriter(fileWritter);
+	         bw.write(data);
+	         bw.close();
+	         System.out.println("Done");
+	      } catch(IOException e){
+	         e.printStackTrace();
+	      }
+	   }
+}
+
